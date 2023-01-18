316100bd403ee310a037423e875e6b981f8c8f42
sana230 <79788020+sana230@users.noreply.github.com>
2023-01-18 23:18:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Sana230/MdiskSearchBotV2.1.git /MdiskSearchBotV2.1
+cd /MdiskSearchBotV2.1
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
